2c19acc18933619db0d9064073db1ec71ba26861
Tran Hoai Nam <namguni2004@gmail.com>
2026-08-22 21:16:15 +0700

    added function in video_stats.py file to get list of video ID

--- video_stats.py
+++ video_stats.py
@@ -9,6 +9,8 @@ API_KEY = os.getenv("API_KEY")
 
 CHANNEL_HANDLE = "MrBeast"
 
+maxResults = 50
+
 
 def get_playlist_id():
 
@@ -38,10 +40,47 @@ def get_playlist_id():
         raise e
 
 
+def get_video_ids(playlistId):
+
+    video_Ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+    try:
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_Ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_Ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 def batch_list(video_id_lst, batch_size):
     for video_id in range(0, len(video_id_lst), batch_size):
         yield video_id_lst[video_id : video_id + batch_size]
 
 
 if __name__ == "__main__":
-    get_playlist_id()
+    playListId = get_playlist_id()
+    print(get_video_ids(playListId))
