17d2395f2da818dcd0057dcf0369ab61288ea4df
Tran Hoai Nam <namguni2004@gmail.com>
2026-09-11 12:48:50 +0700

    done with unit test,  integration test, e2e test and update DAGs

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,18 +1,24 @@
 import requests
+
 import json
-import os
-from dotenv import load_dotenv
+
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
+
 from datetime import date
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = Variable.get("API_KEY")
 
-CHANNEL_HANDLE = "MrBeast"
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 
 maxResults = 50
 
 
+@task
 def get_playlist_id():
 
     try:
@@ -41,6 +47,7 @@ def get_playlist_id():
         raise e
 
 
+@task
 def get_video_ids(playlistId):
 
     video_Ids = []
@@ -77,6 +84,7 @@ def get_video_ids(playlistId):
         raise e
 
 
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -105,7 +113,7 @@ def extract_video_data(video_ids):
 
                 video_data = {
                     "video_id": video_id,
-                    "snippet": snippet["title"],
+                    "title": snippet["title"],
                     "publishedAt": snippet["publishedAt"],
                     "duration": contentDetails["duration"],
                     "viewCount": statistics.get("viewCount", None),
@@ -121,6 +129,7 @@ def extract_video_data(video_ids):
         raise e
 
 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
